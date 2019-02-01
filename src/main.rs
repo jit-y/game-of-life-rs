6cc816f090d5e1b9c0e6216aeb1a0d17ed0be3de
YUJI takahashi <jit.y.31@gmail.com>
2019-02-01 23:11:12 +0900

    render LifeGame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use rand;
+
+struct LifeGame {
+    row: usize,
+    column: usize,
+    grid: Vec<Vec<bool>>,
+}
+
+impl LifeGame {
+    fn new(row: usize, column: usize) -> LifeGame {
+        let mut grid = vec![vec![false; column]; row];
+
+        for r in 0..row {
+            for c in 0..column {
+                if rand::random::<u32>() % 10 == 0 {
+                    grid[r][c] = true
+                }
+            }
+        }
+
+        LifeGame {
+            row: row,
+            column: column,
+            grid: grid,
+        }
+    }
+
+    fn render(&self) {
+        for columns in &self.grid {
+            for val in columns {
+                if *val {
+                    print!("■")
+                } else {
+                    print!(" ")
+                }
+            }
+            println!()
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lg = LifeGame::new(10, 10);
+    lg.render()
 }
